b4a1a1e6632a075aae117ce7ff5a0ba412724fba
Caner Kaya <canerkaya718@gmail.com>
2021-06-02 03:22:08 +0200

    added comments

--- utility.py
+++ utility.py
@@ -7,21 +7,15 @@ class Utility:
 
   @staticmethod
   def readFile(path):
+  #reads and returns the given file
     file = open(path,"r")
     code = file.read()
     file.close()
     return  code
 
-  @staticmethod 
-  def isConsist(var,lists):
-    for aList in lists:
-      if var == aList[2]:
-        return True
-    return False
-
   @staticmethod
   def draw_graph(nodes, edges,edge_labels,size, path=None, save = True):
-
+    #draw networkx graph with given settings
     G = nx.DiGraph()
     G.add_nodes_from(nodes)
     G.add_edges_from(edges)
@@ -33,19 +27,27 @@ class Utility:
       formatted_edge_labels = dict([((n[0], n[1]), edge_labels[i]) for  i, n in enumerate(G.edges(data=True))])
 
     pos = nx.planar_layout(G)
-    nx.draw(G, pos, with_labels=True,node_size=2000,node_color = '#86D3D4',node_shape='o',font_size="15",arrowsize=18)
+    nx.draw(G, pos, with_labels=True,node_size=2250,node_color = '#86D3D4',node_shape='o',font_size="12",arrowsize=18)
     nx.draw_networkx_edge_labels(G, pos = pos,edge_labels=formatted_edge_labels, label_pos=0.5,
                              font_color='red', font_size=10)
-
-
     if save:
       plt.savefig(path,transparent=True)
 
   @staticmethod
   def getLineNumber(pos, document):
+    #Returns the line number to which the character's position corresponds
       return len(re.findall('\n', document[:pos])) + 1 
 
+  @staticmethod 
+  def isConsist(var,lists):
+  #Returns true if the variable is exist inside the nested list's second element
+    for aList in lists:
+      if var == aList[2]:
+        return True
+    return False
+
 class Object:
+  #instance of object
     def __init__(self,variableType, variableName, pos):
       self.pos = pos
       self.variableType = variableType
